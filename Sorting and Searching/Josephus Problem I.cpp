b7bb7914a761ee83dcd381c0f38a8147659e6d80
Md Tazim Uddin <66694987+TazimAh@users.noreply.github.com>
2022-12-13 16:11:34 +0600

    solved

---
+++ Sorting and Searching/Josephus Problem I.cpp
@@ -0,0 +1,63 @@
+/* Author: Tazim(The_crawler) */
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long int
+#define dec greater<int>()
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+#define prow(n) for(auto i:n)cout<<i<<" ";cout<<"\n";
+#define pcol(n) for(auto i:n)cout<<i<<"\n";
+#define yes cout<<"YES"<<"\n";
+#define no cout<<"NO"<<"\n";
+#define all(x) (x).begin(), (x).end()
+#define nl "\n"
+#define INF 2147483647
+#define INFLL 9223372036854775807 
+#define debug(x) cout<<#x<<" = ";_print(x); cout<<endl; /// Debug function
+using namespace std; ///****
+using namespace __gnu_pbds;
+bool sortbysec(const pair<int, int>& a, const pair<int, int>& b) { return (a.second < b.second); }
+template<class T> void _print(T x) { cout << x; }
+template<class T> void _print(vector<T>x) { cout << "[ ";for (T i : x)cout << i << " ";cout << "]"; }
+template<class T> void _print(set<T>x) { cout << "[ ";for (T i : x)cout << i << " ";cout << "]"; }
+template<class T, class V> void _print(pair<T, V>x) { cout << "{" << x.first << "," << x.second << "} "; }
+template<class T, class V> void _print(map<T, V>x) { cout << "[ ";for (auto i : x)_print(i);cout << "]"; }
+template<class T> void _print(multiset<T>x) { cout << "[ ";for (T i : x)cout << i << " ";cout << "]"; }
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+//  less_equal - multiset | greater - decending order unique | less - asc unique | greater_equel - dec multiset
+ 
+/* Hack my code. It's Easy to Read */
+ 
+ 
+int main() {
+   FAST_IO;
+   // start
+      int n, k;
+      cin>>n;
+      k  =n;
+      vector<int> ar(n, 1);
+      for(int i=0; i<n; i++)ar[i]= i+1;
+      int cnt =0;
+      int ind =0;
+      int cur =0;
+      while (cnt<k)
+      {
+         if(ar[ind]!=-1){
+            cur++;
+         }
+         if(cur==2){
+            cnt++;
+            cur=0;
+            cout<<ar[ind]<<" ";
+            ar[ind] = -1;
+         }
+         ind++;
+         if(ind>=n)ind-=n;
+         
+      }
+      
+ 
+   
+   return 0;
+}
