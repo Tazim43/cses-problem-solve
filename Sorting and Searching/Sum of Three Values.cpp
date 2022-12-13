70cafe7afa4feeb1f6136e87121d369a25021e0b
Md Tazim Uddin <66694987+TazimAh@users.noreply.github.com>
2022-12-13 16:13:50 +0600

    solved

---
+++ Sorting and Searching/Sum of Three Values.cpp
@@ -0,0 +1,75 @@
+/* Author: Tazim(The_crawler) */
+#include<bits/stdc++.h>
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long int
+#define dec greater<int>()
+#define lower(s) transform(s.begin(), s.end(), s.begin(), ::tolower);
+#define upper(s) transform(s.begin(), s.end(), s.begin(), ::toupper);
+#define prow(n) for(auto i:n)cout<<i<<" ";cout<<"\n";
+#define pcol(n) for(auto i:n)cout<<i<<"\n";
+#define yes cout<<"YES"<<"\n";
+#define no cout<<"NO"<<"\n";
+#define all(x) (x).begin(), (x).end()
+#define nl "\n"
+#define INF 2147483647
+#define INFLL 9223372036854775807 
+#define debug(x) cout<<#x<<" = ";_print(x); cout<<endl; /// Debug function
+using namespace std; ///****
+bool sortbysec(const pair<int, int>& a, const pair<int, int>& b) { return (a.second < b.second); }
+template<class T> void _print(T x) { cout << x; }
+template<class T> void _print(vector<T>x) { cout << "[ ";for (T i : x)cout << i << " ";cout << "]"; }
+template<class T> void _print(set<T>x) { cout << "[ ";for (T i : x)cout << i << " ";cout << "]"; }
+template<class T, class V> void _print(pair<T, V>x) { cout << "{" << x.first << "," << x.second << "} "; }
+template<class T, class V> void _print(map<T, V>x) { cout << "[ ";for (auto i : x)_print(i);cout << "]"; }
+template<class T> void _print(multiset<T>x) { cout << "[ ";for (T i : x)cout << i << " ";cout << "]"; }
+/* Hack my code. It's Easy to Read */
+ 
+ 
+int main() {
+   FAST_IO;
+   // start
+   int n, k;
+   cin >> n >> k;
+   vector<pair<int, int> > ar(n);
+   for (int i = 0; i < n; i++) {
+      cin >> ar[i].first;
+      ar[i].second = i;
+   }
+   sort(all(ar));
+   for (int i = 0; i < n; i++) {
+      if (ar[i].first >= k)break;
+      int a = k - ar[i].first;
+      int l = 0, r = n - 1;
+      while (l < r)
+      {
+         // debug(l)
+         if (ar[l].second == ar[i].second) {
+            l++;
+            continue;
+         }
+         if (ar[r].second == ar[i].second) {
+            r--;
+            continue;
+         }
+         if (ar[l].first + ar[r].first < a) l++;
+         else if (ar[l].first + ar[r].first == a) {
+            int a1 = ar[l].second, a2 = ar[r].second, a3 = ar[i].second;
+            if (a1 != a2 && a2 != a3 && a1 != a3) {
+               cout << a1 + 1 << " " << a2 + 1 << " " << a3 + 1 << endl;
+               return 0;
+            }
+            else {
+               if (a3 == a1)l++;
+               else r--;
+            }
+ 
+         }
+         else r--;
+      }
+ 
+   }
+   cout << "IMPOSSIBLE" << endl;
+ 
+ 
+   return 0;
+}
